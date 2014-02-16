51cb5dce30f5bab647c137445d417b7677cc82e6
mraypold <mraypold@gmail.com>
2014-02-16 07:59:11 -0700

    Test Project Added

---
+++ Team7ProjectTest/src/ca/ualberta/team7project/test/useCaseValidation.java
@@ -0,0 +1,17 @@
+package ca.ualberta.team7project.test;
+
+import ca.ualberta.team7project.MainActivity;
+import android.test.ActivityInstrumentationTestCase2;
+
+public class useCaseValidation extends
+		ActivityInstrumentationTestCase2<MainActivity> {
+
+	public useCaseValidation(Class<MainActivity> name) {
+		super(name);
+	}
+
+	protected void setUp() throws Exception {
+		super.setUp();
+	}
+
+}
