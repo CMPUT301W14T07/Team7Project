a511de8bcacc5d3abf7d39af7a9d6cd959dc8baf
mraypold <mraypold@gmail.com>
2014-03-07 14:24:05 -0700

    Test for constructor overload
    Fixed forgotten overload in subclasses.

--- Team7ProjectTest/src/ModelTests/TopicThreadModelTest.java
+++ Team7ProjectTest/src/ModelTests/TopicThreadModelTest.java
@@ -20,74 +20,79 @@ import ca.ualberta.team7project.models.TopicModel;
 import ca.ualberta.team7project.models.UserModel;
 
 public class TopicThreadModelTest extends
-	ActivityInstrumentationTestCase2<MainActivity> {
+		ActivityInstrumentationTestCase2<MainActivity>
+{
 
-    public TopicThreadModelTest() {
+	public TopicThreadModelTest()
+	{
 
-	super(MainActivity.class);
-    }
+		super(MainActivity.class);
+	}
 
-    /**
-     * Test to ensure that basic properties of the topic are correct.
-     */
-    public void testTopicCreate() {
+	/**
+	 * Test to ensure that basic properties of the topic are correct.
+	 */
+	public void testTopicCreate()
+	{
 
-	UserModel user = new UserModel("Ash Ketchum");
-	TopicModel topic = new TopicModel("A listing of all my Pokemon", null,
-		user, null, "Ash's pokedex");
+		UserModel user = new UserModel("Ash Ketchum");
+		TopicModel topic = new TopicModel("A listing of all my Pokemon", null,
+				user, null, "Ash's pokedex");
 
-	assertEquals("Topic title is correct", "Ash's pokedex",
-		topic.getTitle());
-	assertEquals("Author name is correct", "Ash Ketchum",
-		topic.getAuthorName());
-	assertEquals("Unique author name is the same as the users",
-		topic.getAuthorUnique(), user.getUniqueName());
-    }
+		assertEquals("Topic title is correct", "Ash's pokedex",
+				topic.getTitle());
+		assertEquals("Author name is correct", "Ash Ketchum",
+				topic.getAuthorName());
+		assertEquals("Unique author name is the same as the users",
+				topic.getAuthorUnique(), user.getUniqueName());
+	}
 
-    /**
-     * Test to determine that chronological insertion into Topics is correct
-     */
-    public void testThreadsInsertedProperly() {
+	/**
+	 * Test to determine that chronological insertion into Topics is correct
+	 */
+	public void testThreadsInsertedProperly()
+	{
 
-	UserModel user = new UserModel("Ash Ketchum");
-	TopicModel topic = new TopicModel("A listing of all my Pokemon", null,
-		user, null, "Ash's pokedex");
+		UserModel user = new UserModel("Ash Ketchum");
+		TopicModel topic = new TopicModel("A listing of all my Pokemon", null,
+				user, null, "Ash's pokedex");
 
-	CommentModel threadOne = new CommentModel("Caught Snorelax", null,
-		user, null);
+		CommentModel threadOne = new CommentModel("Caught Snorelax", null,
+				user, null);
 
-	topic.setLastThreadUniqueID();
-	threadOne.setUniqueID(topic.getLastThreadUniqueID());
-	topic.addComment(threadOne);
+		topic.setLastThreadUniqueID();
+		threadOne.setUniqueID(topic.getLastThreadUniqueID());
+		topic.addComment(threadOne);
 
-	CommentModel threadTwo = new CommentModel("Caught Mew", null, user,
-		null);
+		CommentModel threadTwo = new CommentModel("Caught Mew", null, user,
+				null);
 
-	topic.setLastThreadUniqueID();
-	threadTwo.setUniqueID(topic.getLastThreadUniqueID());
-	topic.addComment(threadTwo);
+		topic.setLastThreadUniqueID();
+		threadTwo.setUniqueID(topic.getLastThreadUniqueID());
+		topic.addComment(threadTwo);
 
-	CommentModel threadThree = new CommentModel("Caught Jigglypuff", null,
-		user, null);
+		CommentModel threadThree = new CommentModel("Caught Jigglypuff", null,
+				user, null);
 
-	topic.setLastThreadUniqueID();
-	threadThree.setUniqueID(topic.getLastThreadUniqueID());
-	topic.addComment(threadThree);
+		topic.setLastThreadUniqueID();
+		threadThree.setUniqueID(topic.getLastThreadUniqueID());
+		topic.addComment(threadThree);
 
-	LinkedList<ThreadModel> pokedexThreads = new LinkedList<ThreadModel>();
-	pokedexThreads.add(threadOne);
-	pokedexThreads.add(threadTwo);
-	pokedexThreads.add(threadThree);
+		LinkedList<ThreadModel> pokedexThreads = new LinkedList<ThreadModel>();
+		pokedexThreads.add(threadOne);
+		pokedexThreads.add(threadTwo);
+		pokedexThreads.add(threadThree);
 
-	assertEquals("Threads have been correctly entered", pokedexThreads,
-		topic.getComments());
-    }
+		assertEquals("Threads have been correctly entered", pokedexThreads,
+				topic.getComments());
+	}
 
-    /**
-     * Test to determine that Location is properly set for the topic
-     */
-    public void testTopicLocation() {
+	/**
+	 * Test to determine that Location is properly set for the topic
+	 */
+	public void testTopicLocation()
+	{
 
-	// TODO
-    }
+		// TODO
+	}
 }
