731b978c1c5d3449d913daa356281ce74827fe52
EdenMar <emar@ualberta.ca>
2014-03-16 18:59:07 -0600

    Added more tests for LocationModel

--- Team7ProjectTest/src/ca/ualberta/team7project/tests/LocationModelTest.java
+++ Team7ProjectTest/src/ca/ualberta/team7project/tests/LocationModelTest.java
@@ -15,17 +15,44 @@ public class LocationModelTest extends ActivityInstrumentationTestCase2<MainActi
 
 	public void testGetLongitude()
 	{
-		LocationModel lm = new LocationModel(25.0, 25.0);
+		LocationModel lm = new LocationModel(0.0, 0.0);
 		
 		
-		assertEquals("Should be 25.0", 25.0, lm.getLongitude());
+		assertEquals("Should be 0.0", 0.0, lm.getLongitude());
 	}
 	
 	public void testGetLatitude()
 	{
-		LocationModel lm = new LocationModel(65, 55);
+		LocationModel lm = new LocationModel(650000000.0, 1000000000.5);
 		
-		assertEquals("Should be 55.0", 55, lm.getLatitude());
+		assertEquals("Should be 1000000000.5", 1000000000.5, lm.getLatitude());
+		
+	}
+	
+	public void testSetLongitude()
+	{
+		LocationModel lm = new LocationModel(55, 60);
+		lm.setLongitude(99.0);
+		
+		assertEquals("This should be 99.0", 99.0, lm.getLongitude());
+		
+	}
+	
+	public void testSetLatitude()
+	{
+		LocationModel lm = new LocationModel(1000000, 500000);
+		lm.setLatitude(9);
+		
+		assertEquals("This should be 9.0 now", 9.0, lm.getLatitude(), 0.0);
+	}
+	
+	public void testSetLocation()
+	{
+		LocationModel lm = new LocationModel(9000, 5000);
+		lm.setLocation(1, 2);
+		
+		assertEquals("New long is 1.0", 1.0, lm.getLongitude());
+		assertEquals("New lat is 2.0,", 2.0, lm.getLatitude());
 		
 	}
 }
