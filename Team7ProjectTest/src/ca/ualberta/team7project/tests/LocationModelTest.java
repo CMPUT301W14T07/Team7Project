f11b7f626be66b9700a81647be0568722d7a8bd9
EdenMar <emar@ualberta.ca>
2014-03-16 18:24:03 -0600

    Added a LocationModel test

--- Team7ProjectTest/src/ca/ualberta/team7project/tests/LocationModelTest.java
+++ Team7ProjectTest/src/ca/ualberta/team7project/tests/LocationModelTest.java
@@ -1,6 +1,7 @@
 package ca.ualberta.team7project.tests;
 
 import ca.ualberta.team7project.MainActivity;
+import ca.ualberta.team7project.models.LocationModel;
 import android.test.ActivityInstrumentationTestCase2;
 
 
@@ -12,4 +13,19 @@ public class LocationModelTest extends ActivityInstrumentationTestCase2<MainActi
 		super(MainActivity.class);
 	}
 
+	public void testGetLongitude()
+	{
+		LocationModel lm = new LocationModel(25.0, 25.0);
+		
+		
+		assertEquals("Should be 25.0", 25.0, lm.getLongitude());
+	}
+	
+	public void testGetLatitude()
+	{
+		LocationModel lm = new LocationModel(65, 55);
+		
+		assertEquals("Should be 55.0", 55, lm.getLatitude());
+		
+	}
 }
