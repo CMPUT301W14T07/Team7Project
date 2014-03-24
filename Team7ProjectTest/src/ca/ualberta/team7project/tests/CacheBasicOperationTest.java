c5a5bb789cf8d54cc38a95e969f1c242f6ade6a0
johnzw <rzzw456@126.com>
2014-03-24 17:28:38 -0600

    Test passed!!

--- Team7ProjectTest/src/ca/ualberta/team7project/tests/CacheBasicOperationTest.java
+++ Team7ProjectTest/src/ca/ualberta/team7project/tests/CacheBasicOperationTest.java
@@ -4,26 +4,42 @@ import android.test.ActivityInstrumentationTestCase2;
 import ca.ualberta.team7project.MainActivity;
 import ca.ualberta.team7project.cache.MemoryToFileOperation;
 import ca.ualberta.team7project.cache.ThreadModelPool;
+import ca.ualberta.team7project.models.ThreadModel;
 
 public class CacheBasicOperationTest extends
 		ActivityInstrumentationTestCase2<MainActivity> {
+	
+	private MainActivity ac;
 
 	public CacheBasicOperationTest()
 	{
 		super(MainActivity.class);
 	}
 	
-	//I 
+	
 	public void testSaveInFileWithPoolEmpty(){
 		
-		//populate pool with several examples
-		//ThreadModel tm = new ThreadModel("some text", null, "some text too");
-		
-		MemoryToFileOperation tool = new MemoryToFileOperation(new MainActivity());
+		ac = this.getActivity();
+		MemoryToFileOperation tool = new MemoryToFileOperation(ac);
 		tool.saveInFile();
 		tool.loadFromFile();
 		
 		this.assertEquals(true, ThreadModelPool.threadModelPool.size() == 0);
+	}
+
+	
+	public void testSaveInFilePupulated(){
+		ac = this.getActivity();
+		
+		//populate pool with several examples
+		ThreadModel tm = new ThreadModel("some text", null, "some text too");
+		MemoryToFileOperation tool = new MemoryToFileOperation(ac);
+		ThreadModelPool.threadModelPool.add(tm);
+		
+		tool.saveInFile();
+		tool.loadFromFile();
+		
+		this.assertEquals(true, ThreadModelPool.threadModelPool.size() == 1);
 	}
 	
 	
