6217c722ce9d711bf857fdff5ef080a9e9756086
mraypold <mraypold@gmail.com>
2014-03-26 08:54:54 -0600

    LocationTesting

--- Team7ProjectTest/src/ca/ualberta/team7project/tests/LocationTest.java
+++ Team7ProjectTest/src/ca/ualberta/team7project/tests/LocationTest.java
@@ -2,37 +2,80 @@ package ca.ualberta.team7project.tests;
 
 import android.test.ActivityInstrumentationTestCase2;
 import ca.ualberta.team7project.MainActivity;
+import ca.ualberta.team7project.controllers.LocationController;
+import ca.ualberta.team7project.controllers.UserController;
 import ca.ualberta.team7project.models.LocationModel;
+import ca.ualberta.team7project.models.PreferenceModel;
+import ca.ualberta.team7project.models.UserModel;
 
 
 public class LocationTest extends
 		ActivityInstrumentationTestCase2<MainActivity>
 {
 
+	private MainActivity mainActivity;
+	
 	public LocationTest()
 	{
 		super(MainActivity.class);
+		mainActivity = getActivity();
 	}
     
-	/*
-	 * When the user inputs and alternate location, the value should be set in the controller
+	/**
+	 * Ensure that address lookup is returning a non null result and that the location is properly set
 	 */
 	public void testAlternateLocation()
 	{
-		MainActivity.getLocationController().addressLookup("University of Alberta");
+		LocationController locationController = mainActivity.getLocationController();
+		
+		/* Set alternate location to null before beginning test so that we can check against it */
+		locationController.setAlternateLocation(null);
+		
+		locationController.addressLookup("University of Alberta");		
 				
-		LocationModel location = MainActivity.getLocationController().getAlternateLocation();
-		
-		assertNotNull("location not null", location);
-		
-		/* geocoder coordinates for uofa are ~53 for the latitude and ~-113 fore longitude */
-		double latitude = location.getLatitude();
-		
-		boolean result = false;
-		
-		if(latitude > 50 & latitude < 55)
-			result = true;
-	
-		assertTrue("locations were set", result);
+		/* Wait a reasonable time for the thread to finish. Perhaps there is a better way to do this? */
+		try
+		{
+			Thread.sleep(1000);
+		} catch (InterruptedException e)
+		{
+			e.printStackTrace();
+		}
+
+		/* Once the thread completes the alternate location should not be null */
+		LocationModel locationModel = locationController.getAlternateLocation();
+		assertNotNull("location not null", locationModel);
+		assertNotNull("Alerternate location is not null", locationController.getAlternateLocation());
 	}
+	
+	/**
+	 * Forcing a location update should result in a new LocationModel in the controller and UserModel should be updated
+	 */
+	public void testForceLocationUpdate()
+	{
+		LocationController locationController = mainActivity.getLocationController();
+		
+		/* Save the old locationModel to test against the one set in the forced update */
+		LocationModel oldModel = locationController.getUserLocation();
+		
+		locationController.forceLocationUpdate();
+		/* Wait a reasonable time for the thread to finish. Perhaps there is a better way to do this? */
+		try
+		{
+			Thread.sleep(1000);
+		} catch (InterruptedException e)
+		{
+			e.printStackTrace();
+		}
+		
+		LocationModel newModel = locationController.getUserLocation();
+		assertFalse("Location models should be different", newModel.equals(oldModel));
+		
+		UserController userController = mainActivity.getUserController();
+		PreferenceModel preference = userController.getUser();
+		UserModel user = preference.getUser();
+		
+		assertTrue("User location was updated with the force update", newModel.equals(user.getLocation()));
+	}
+
 }
