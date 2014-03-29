4c38b07812cd4f95bfe772fc35e731424251b6cc
EdenMar <emar@ualberta.ca>
2014-03-29 15:59:09 -0600

    Finished testing CacheOperation

--- Team7ProjectTest/src/ca/ualberta/team7project/tests/CacheOperationTest.java
+++ Team7ProjectTest/src/ca/ualberta/team7project/tests/CacheOperationTest.java
@@ -25,7 +25,7 @@ public class CacheOperationTest extends ActivityInstrumentationTestCase2<MainAct
 		
 		//to make file empty
 		tool.saveFile(ac);
-		this.assertEquals(true, ThreadModelPool.threadModelPool.size() == 0);
+		assertEquals(true, ThreadModelPool.threadModelPool.size() == 0);
 	}
 	
 	//at this point, pool in the file only 0 threadmodel
@@ -42,7 +42,7 @@ public class CacheOperationTest extends ActivityInstrumentationTestCase2<MainAct
 		
 		//This test doesn't really test anything outside of the .saveThread method, 
 		//something that isn't in CacheOperation.java
-		this.assertEquals(true, ThreadModelPool.threadModelPool.size() == 1);
+		assertEquals(true, ThreadModelPool.threadModelPool.size() == 1);
 		
 		//this test clears the pool in our app and loads what has been saved previously
 		tool.loadFile(ac);
@@ -67,7 +67,7 @@ public class CacheOperationTest extends ActivityInstrumentationTestCase2<MainAct
 		
 		//again, this doesn't test a method in CacheOperation.java. We know that
 		//saveThread works, so this test is just a repeat of test2 
-		this.assertEquals(true, ThreadModelPool.threadModelPool.size() == 2);
+		assertEquals(true, ThreadModelPool.threadModelPool.size() == 2);
 		
 	}
 	
@@ -79,7 +79,7 @@ public class CacheOperationTest extends ActivityInstrumentationTestCase2<MainAct
 		//load the pool from file to memory
 		tool.loadFile(ac);
 		
-		this.assertEquals(true, ThreadModelPool.threadModelPool.size() == 1);
+		assertEquals(true, ThreadModelPool.threadModelPool.size() == 1);
 	}
 	
 	//at this point, pool in the file has 1 threadModel
@@ -98,14 +98,14 @@ public class CacheOperationTest extends ActivityInstrumentationTestCase2<MainAct
 		
 		//add a collection of threadModel(which are 2 of them) to the cache in memory
 		tool.saveCollection(collection);
-		this.assertEquals(true, ThreadModelPool.threadModelPool.size() == 3);
+		assertEquals(true, ThreadModelPool.threadModelPool.size() == 3);
 		
 		//synchronize File sytem with pool in the memory
 		tool.saveFile(ac);
 		
 		//Synchronize pool in the memory with the pool in the file
 		tool.loadFile(ac);
-		this.assertEquals(true, ThreadModelPool.threadModelPool.size() == 3);
+		assertEquals(true, ThreadModelPool.threadModelPool.size() == 3);
 			
 	}
 	
@@ -126,13 +126,56 @@ public class CacheOperationTest extends ActivityInstrumentationTestCase2<MainAct
 		//save into cache in memory
 		tool.saveCollection(collection);
 		
+		//not entirely convinced these test hierarchy
 		Collection<ThreadModel> c1 = tool.searchByUUID(UUID.fromString(ThreadModel.ROOT), CacheOperation.BY_PARENTID);
 		Collection<ThreadModel> c2 = tool.searchByUUID(tm.getUniqueID(), CacheOperation.BY_ITSELFID);
 		Collection<ThreadModel> c3 = tool.searchByUUID(tm2.getTopicUUID(), CacheOperation.BY_TOPICID);
 		
-		this.assertEquals(true, c1.size() == 5);
-		this.assertEquals(true, c2.size() == 1);
-		this.assertEquals(true, c3.size() == 1);
+		assertEquals(true, c1.size() == 5);
+		assertEquals(true, c2.size() == 1);
+		assertEquals(true, c3.size() == 1);
+
+		Collection<ThreadModel> collection2 = new ArrayList<ThreadModel>();
+		ThreadModel topBlue = new ThreadModel ("Blastoise", null, "Blue's team");
+		ThreadModel midBlue1 = new ThreadModel("Wortortle", null, topBlue.getUniqueID(), topBlue.getUniqueID());
+		ThreadModel midBlue2 = new ThreadModel("Poliwhorl", null, topBlue.getUniqueID(), topBlue.getUniqueID());
+		ThreadModel midBlue3 = new ThreadModel("Vaporeon", null, topBlue.getUniqueID(), topBlue.getUniqueID());
+		ThreadModel lowBlue = new ThreadModel("Squirtle", null, midBlue1.getUniqueID(), topBlue.getUniqueID());
+
+		collection2.add(topBlue);
+		collection2.add(midBlue3);
+		collection2.add(midBlue2);
+		collection2.add(midBlue1);
+		collection2.add(lowBlue);
+		
+		tool.saveCollection(collection2);
+		assertEquals("Should have 10 total now", 10, ThreadModelPool.threadModelPool.size());
+		
+		//search1: find all the threads with topBlue as the topic, should have 5
+		Collection<ThreadModel> search1 = tool.searchByUUID(topBlue.getUniqueID(), CacheOperation.BY_TOPICID);
+		assertEquals("Should have 5", 5, search1.size());
+		for (ThreadModel element : search1)
+		{
+			assertEquals("Expecting every Pokemon to be here", true, collection2.contains(element));
+		}
+		
+		//search2: find all the direct children of topBlue, should have 3
+		Collection<ThreadModel> search2 = tool.searchByUUID(topBlue.getUniqueID(), CacheOperation.BY_PARENTID);
+		assertEquals("Should have 3", 3, search2.size());
+		for (ThreadModel element2 : search2)
+		{
+			assertEquals("Expecting mid tier Pokemon", true, collection2.contains(element2));
+		}
+		
+		//search3: find lowBlue
+		Collection<ThreadModel> search3 = tool.searchByUUID(lowBlue.getUniqueID(), CacheOperation.BY_ITSELFID);
+		assertEquals("Should have 1", 1, search3.size());
+		for (ThreadModel element3 : search3)
+		{
+			assertEquals("Expecting low tier", true, collection2.contains(element3));
+		}
+		
+		
 			
 	}
 	
@@ -145,7 +188,7 @@ public class CacheOperationTest extends ActivityInstrumentationTestCase2<MainAct
 		//to make file empty
 		tool.saveFile(ac);
 		tool.loadFile(ac);
-		this.assertEquals(true, ThreadModelPool.threadModelPool.size() == 0);
+		assertEquals(true, ThreadModelPool.threadModelPool.size() == 0);
 	}
 	
 	
