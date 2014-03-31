596b19b6f56b6072e4ea241b578aafd77cb52268
EdenMar <emar@ualberta.ca>
2014-03-30 18:30:32 -0600

    Added fetchDescendent and tests

--- Team7ProjectTest/src/ca/ualberta/team7project/tests/ThreadFetcherTests.java
+++ Team7ProjectTest/src/ca/ualberta/team7project/tests/ThreadFetcherTests.java
@@ -11,27 +11,25 @@ import ca.ualberta.team7project.network.ThreadFetcher.SortMethod;
 
 //There exists a top level comment, it's reply, and a reply to that reply. They
 //will be denoted as top, mid and low respectively, and their variables are self
-//parent and topic
+//parent and topic. From the ES server, the comments are "Moltres", "Flareon" and 
+//Ponyta. By using known values that have been created previously, can eliminate 
+//need to create new test cases for the test everytime
 
 public class ThreadFetcherTests extends ActivityInstrumentationTestCase2<MainActivity> 
 {
 
-	/*
-	public UUID topSelf = UUID.fromString("62a0e338-4743-4b2e-83b6-570d097cccb8");
-	public UUID topParent =UUID.fromString("db352350-aa82-11e3-a5e2-0800200c9a66"); //root
-	public UUID topTopic = UUID.fromString("62a0e338-4743-4b2e-83b6-570d097cccb8");
+
+	public UUID topSelf = UUID.fromString("dc42282b-7667-4cb4-ac9e-6ebb5a159d84");
+	public UUID topParent = UUID.fromString("db352350-aa82-11e3-a5e2-0800200c9a66"); //root
+	public UUID topTopic = UUID.fromString("dc42282b-7667-4cb4-ac9e-6ebb5a159d84");
 	
-	public UUID midSelf = UUID.fromString("56c6ee32-ce1e-4fb3-8b2c-532864b35e03");
-	public UUID midParent = UUID.fromString("62a0e338-4743-4b2e-83b6-570d097cccb8");
-	public UUID midTopic = UUID.fromString("56c6ee32-ce1e-4fb3-8b2c-532864b35e03");
+	public UUID midSelf = UUID.fromString("0730a8e9-9610-48db-855f-0e20948117b9");
+	public UUID midParent = UUID.fromString("dc42282b-7667-4cb4-ac9e-6ebb5a159d84");
+	public UUID midTopic = UUID.fromString("dc42282b-7667-4cb4-ac9e-6ebb5a159d84");
 	
-	public UUID lowSelf = UUID.fromString("02f219bc-c6c8-45b6-86cf-8e59cae76ed3");
-	public UUID lowParent = UUID.fromString("56c6ee32-ce1e-4fb3-8b2c-532864b35e03");
-	public UUID lowTopic = UUID.fromString("02f219bc-c6c8-45b6-86cf-8e59cae76ed3");
-	*/
-	//
-	public UUID search = UUID.fromString("f6d73367-d471-4613-9fc8-3d32c8f38385");
-	
+	public UUID lowSelf = UUID.fromString("e649ad98-c369-45c9-ad2b-9fa105ec3287");
+	public UUID lowParent = UUID.fromString("0730a8e9-9610-48db-855f-0e20948117b9");
+	public UUID lowTopic = UUID.fromString("dc42282b-7667-4cb4-ac9e-6ebb5a159d84");
 	public ThreadFetcherTests()
 	{
 		super(MainActivity.class);
@@ -41,13 +39,22 @@ public class ThreadFetcherTests extends ActivityInstrumentationTestCase2<MainAct
 	{
 		ThreadFetcher fetcher = new ThreadFetcher();
 		
-		
-		ArrayList<ThreadModel> threads = fetcher.fetchChildComments(search, SortMethod.DATE);
-		
-		
-		assertNotNull(threads);
-		assertEquals("1?", 1, threads.size());
-		assertEquals("2?", 2, threads.size());
+		//should have no children
+		ArrayList<ThreadModel> threads = fetcher.fetchChildComments(lowSelf, SortMethod.DATE);				
+		assertEquals("Should be empty", 0, threads.size());
+
+		//should only have one child
+		ArrayList<ThreadModel> threads2 = fetcher.fetchChildComments(topSelf, SortMethod.DATE);
+		assertEquals("Should have only 1", 1, threads2.size());
+	}
+	
+	public void testFetchDescendentComments()
+	{
+		ThreadFetcher fetcher = new ThreadFetcher();
+		//should have two descendents, but because of topic itself it returns 3, don't know if 
+		//this is considered correct behavior yet
+		ArrayList<ThreadModel> threads3 = fetcher.fetchAllDescendents(topSelf, SortMethod.DATE);
+		assertEquals("Should have 2", 2, threads3.size());
 		
 	}
 }
